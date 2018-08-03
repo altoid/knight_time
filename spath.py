51864cecab61b1d1f814e60e794d109848cae990
Doug Tomm <dtomm@radiumone.com>
2018-08-02 22:40:31 -0700

    pruning optimization - do not allow current path to be longer that current min

--- spath.py
+++ spath.py
@@ -27,9 +27,10 @@
 # if all of our moves goes to a 'V' square then the path is no good.
 
 state = []
-NSQUARES = 5
+NSQUARES = 8
 all_moves = {}
 path_count = 0
+min_path_len = NSQUARES * NSQUARES + 1
 
 def show_state(state):
     for r in xrange(NSQUARES):
@@ -51,21 +52,26 @@ def next_moves(loc):
     return all_moves[loc]
 
 
-def find_path(current_path, move):
+def find_path(current_path, move, level):
     global path_count
+    global min_path_len
+
+    if level > min_path_len:
+        return
 
     # figure out every path to the 'e' square from here.  return a list of paths (list of list of (r, c)
     if state[move[0]][move[1]] == 'e':
         path_count += 1
+        min_path_len = min(min_path_len, level)
         current_path.append(move)
-        print current_path
+        print min_path_len
         current_path.pop()
         return
 
     current_path.append(move)
     for m in next_moves(move):
         if state[m[0]][m[1]] == 'e':
-            find_path(current_path, m)
+            find_path(current_path, m, level + 1)
             break
 
         if state[m[0]][m[1]] != 0:
@@ -73,7 +79,7 @@ def find_path(current_path, move):
 
         state[m[0]][m[1]] = 'v'
 
-        find_path(current_path, m)
+        find_path(current_path, m, level + 1)
 
         state[m[0]][m[1]] = 0
     current_path.pop()
@@ -88,5 +94,5 @@ def shortest_path(from_loc, to_loc):
     state[from_loc[0]][from_loc[1]] = 's'
     state[to_loc[0]][to_loc[1]] = 'e'
     show_state(state)
-    find_path([], from_loc)
+    find_path([], from_loc, 0)
     print "done, found %s paths" % path_count
